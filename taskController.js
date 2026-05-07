f06b2fed9cb624f6d89e6a0cb531317d0faf7b08
aishwaryambbug <aishwaryamb077@gmail.com>
2026-05-07 08:24:57 +0530

    Add files via upload

---
+++ taskController.js
@@ -0,0 +1,49 @@
+const Task = require("../models/Task");
+
+// CREATE TASK
+exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// GET TASKS
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// DELETE TASK
+exports.deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: "Task deleted"
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// UPDATE TASK
+exports.updateTask = async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
